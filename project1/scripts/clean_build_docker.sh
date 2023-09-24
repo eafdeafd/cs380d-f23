f14418f19b83e6b1dcd6601c8df79c9ab16fde79
Andrew Wu <andrew.m.wu@gmail.com>
2023-09-24 00:42:33 -0500

    First commit

--- project1/scripts/clean_build_docker.sh
+++ project1/scripts/clean_build_docker.sh
@@ -4,16 +4,16 @@ sudo docker image rm $(sudo docker image ls --format '{{.Repository}} {{.ID}}' |
 
 cd dockerfiles
 
-sudo docker build . -f base.dockerfile -t sekwonlee/kvs:base
-sudo docker push sekwonlee/kvs:base
+sudo docker build . -f base.dockerfile -t eafdeafd/kvs:base
+sudo docker push eafdeafd/kvs:base
 
-sudo docker build . -f client.dockerfile -t sekwonlee/kvs:client
-sudo docker push sekwonlee/kvs:client
+sudo docker build . -f client.dockerfile -t eafdeafd/kvs:client
+sudo docker push eafdeafd/kvs:client
 
-sudo docker build . -f frontend.dockerfile -t sekwonlee/kvs:frontend
-sudo docker push sekwonlee/kvs:frontend
+sudo docker build . -f frontend.dockerfile -t eafdeafd/kvs:frontend
+sudo docker push eafdeafd/kvs:frontend
 
-sudo docker build . -f server.dockerfile -t sekwonlee/kvs:server
-sudo docker push sekwonlee/kvs:server
+sudo docker build . -f server.dockerfile -t eafdeafd/kvs:server
+sudo docker push eafdeafd/kvs:server
 
 cd ..
